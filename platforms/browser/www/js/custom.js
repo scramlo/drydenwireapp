3616b0b17c238f9229d8490b24641e0394d9ca8d
Brian Scramlin <scramlo@hotmail.com>
2017-03-27 14:24:25 -0400

    most of load more button working

--- platforms/browser/www/js/custom.js
+++ platforms/browser/www/js/custom.js
@@ -6,8 +6,11 @@ ARTICLES
 
 var articlesCount;
 
-//this will contain the articles window html markup
-var articlesWindowHTML = [];
+//this will contain the articles window html markup array
+var articlesWindowHTMLArr = [];
+
+//this will contain the articles window html markup concantenated
+var articlesWindowHTML = "";
 
 //this will contain the body of the articles
 var articlesBodyArray = [];
@@ -18,56 +21,76 @@ var articlesListArray = [];
 //base API address
 var drydenwireJSON = "http://drydenwire.com/api/json-articles/";
 
+//This will go up by 10 every time a request is made. so when
+//the user clicks "Load More" it always has ProcessWire start pulling in
+//articles from the next ten.
+var retrieveStart = 0;
+
 //make the query and parse it
-$.ajax({
-  dataType: "json",
-  url: drydenwireJSON,
-  success: function(json) {
-    //console.log(json);
+getArticles(retrieveStart);
 
-    //number of series
-    articlesCount = Object.keys(json).length;
+//Functions and Stuff
 
-    //iterate through each series
-    for (var i = 0; i < articlesCount; i++) {
+//Create get articles function
+function getArticles(startArticle) {
+  $.ajax({
+    dataType: "json",
+    type: "POST",
+    data : {
+      startResults : startArticle // will be accessible in $_POST['startResults']
+    },
+    url: drydenwireJSON,
+    success: function(json) {
+      //console.log(json);
 
-      //Create simple variables for articles.
-      var articleTitle = json[i].title;
-      var articleImageURL = json[i].fImageUrl;
-      var articleDate = json[i].date;
-      var articleBody = json[i].body;
+      //up the retrieveCount counter
+      retrieveStart += 10;
 
-      //add the full URL path to body images. Can't automate this in ProcessWire
-      var articleBody = articleBody.replace(/\/site/g, "http://drydenwire.com/site");
+      //number of articles
+      articlesCount = Object.keys(json).length;
 
-      //This is a card style layout for the articles list.
-      //It is pushed to an array which will be joined together as HTML.
-      articlesWindowHTML.push("<div class='card'>");
-      articlesWindowHTML.push("<img class='card-img-top' src='" + articleImageURL + "'>");
-      articlesWindowHTML.push("<div class='card-block'>");
-      articlesWindowHTML.push("<small>" + articleDate + "</small>");
-      articlesWindowHTML.push("<h4 class='card-title'>" + articleTitle + "</h4>");
-      articlesWindowHTML.push("<button onclick='showArticle(" + i + ")' class='btn-primary btn-block article-button'>View Article</button>");
-      articlesWindowHTML.push("</div><!--End Card Block -->");
-      articlesWindowHTML.push("</div><!--End Card -->");
+      //iterate through each series
+      for (var i = 0; i < articlesCount; i++) {
 
-      //push article head and body to single article array
-      articlesBodyArray.push(
-        "<h3 class='text-xs-center'>" + articleTitle + "</h3>" + "<hr>"
-        + "<img src='" + articleImageURL + "'>" + "<hr>"
-        + articleBody
-      );
-    }
+        //Create simple variables for articles.
+        var articleTitle = json[i].title;
+        var articleImageURL = json[i].fImageUrl;
+        var articleDate = json[i].date;
+        var articleBody = json[i].body;
 
-    //Join the array together.
-    articlesWindowHTML = articlesWindowHTML.join("");
+        //add the full URL path to body images. Can't automate this in ProcessWire
+        var articleBody = articleBody.replace(/\/site/g, "http://drydenwire.com/site");
 
-    //After the splash screen, the first thing you see is the series window
-    $("#articles-window").html(articlesWindowHTML);
+        //This is a card style layout for the articles list.
+        //It is pushed to an array which will be joined together as HTML.
+        articlesWindowHTMLArr.push("<div class='card'>"); //0
+        articlesWindowHTMLArr.push("<img class='card-img-top' src='" + articleImageURL + "'>"); //1
+        articlesWindowHTMLArr.push("<div class='card-block'>"); //2
+        articlesWindowHTMLArr.push("<small>" + articleDate + "</small>"); //3
+        articlesWindowHTMLArr.push("<h4 class='card-title'>" + articleTitle + "</h4>"); //4
+        articlesWindowHTMLArr.push("<button onclick='showArticle(" + i + ")' class='btn-primary btn-block article-button'>View Article</button>"); //5
+        articlesWindowHTMLArr.push("</div><!--End Card Block -->"); //6
+        articlesWindowHTMLArr.push("</div><!--End Card -->"); //7
+        articlesWindowHTMLArr.push("<section id=\"load-more-section\"><button id=\"load-more-button\" class=\"btn btn-success btn-block\" onclick=\"getArticles(retrieveStart);\">Load More</button><br></section>"); //8
 
-  } //end success
+        //push article head and body to single article array
+        articlesBodyArray.push(
+          "<h3 class='text-xs-center'>" + articleTitle + "</h3>" + "<hr>"
+          + "<img src='" + articleImageURL + "'>" + "<hr>"
+          + articleBody
+        );
+      } //End for each loop
 
-}); // End AJAX
+      //Join the array together.
+      articlesWindowHTML = articlesWindowHTMLArr.join("");
+
+      //After the splash screen, the first thing you see is the series window
+      $("#articles-window").html(articlesWindowHTML);
+
+    } //end success
+
+  }); // End AJAX
+}
 
 function showArticle(id) {
   eraseWindow();
